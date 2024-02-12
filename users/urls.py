da4e9d4129d080a49fd55b364620d19dfe36846a
Polatbek <polatbekbeknazarov03@gmail.com>
2024-02-12 15:03:42 +0500

    added login view

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import register
+from .views import register, login_user
 
 urlpatterns = [
     path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 
 from .forms import RegisterForm
 
@@ -8,7 +9,7 @@ def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
 
-        if form.is_valid:
+        if form.is_valid():
             form.save()
 
             username = form.cleaned_data.get('username')
@@ -22,4 +23,27 @@ def register(request):
     else:
         form = RegisterForm()
 
+
     return render(request, 'users/register.html', {'form': form})
+
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+
+                return redirect('index')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'users/login.html', {'form': form})
+
+
